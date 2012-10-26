ddb5e111ce4f2c9f6426d1494baad6c6e74903a3
Sage Weil <sage@inktank.com>
2012-10-25 21:25:19 -0700

    install: ceph-common and ceph-fs-common, too
    Signed-off-by: Sage Weil <sage@inktank.com>

--- ceph_deploy/install.py
+++ ceph_deploy/install.py
@@ -95,6 +95,8 @@ def install_ubuntu(codename, version_kind, version):
             '--assume-yes',
             '--',
             'ceph',
+            'ceph-common',
+            'ceph-fs-common',
             # ceph only recommends gdisk, make sure we actually have
             # it; only really needed for osds, but minimal collateral
             'gdisk',
