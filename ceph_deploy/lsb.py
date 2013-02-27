502acf2c0f0d31636ab739df6eff072dea9ac3e5
Sage Weil <sage@inktank.com>
2013-02-27 14:39:29 -0800

    lsb: use lsb_release tool, not platform module
    platform is giving me bad results on my wheezy box.
    
    Note that at this point, things are working for Debian wheezy!
    
    Signed-off-by: Sage Weil <sage@inktank.com>

--- ceph_deploy/lsb.py
+++ ceph_deploy/lsb.py
@@ -1,16 +1,31 @@
 
 def lsb_release():
-    import platform
-    out = platform.dist()
+    import subprocess
+
+    args = [
+        'lsb_release',
+        '-s',
+        '-i',
+        '-r',
+        '-c',
+        ]
+    p = subprocess.Popen(
+        args=args,
+        stdout=subprocess.PIPE,
+        )
+    out = p.stdout.read()
+    ret = p.wait()
+    if ret != 0:
+        raise subprocess.CalledProcessError(ret, args, output=out)
 
     try:
-        (distro, release, codename) = out
+        (distro, release, codename, empty) = out.split('\n', 3)
     except ValueError:
-        raise RuntimeError('platform.dist() gave invalid output')
-    if distro == '':
-        raise RuntimeError('platform.dist() gave invalid output')
-
+        raise RuntimeError('lsb_release gave invalid output')
+    if empty != '':
+        raise RuntimeError('lsb_release gave invalid output')
     return (distro, release, codename)
+
 
 def choose_init(distro, codename):
     if distro == 'Ubuntu':
