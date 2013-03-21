49edb48c8f5c9db10618e2c3b3a34a83077178e7
Danny Al-Gaaf <danny.al-gaaf@bisect.de>
2013-03-21 11:27:11 +0100

    gatherkeys.py: fix bad indention
    Signed-off-by: Danny Al-Gaaf <danny.al-gaaf@bisect.de>

--- ceph_deploy/gatherkeys.py
+++ ceph_deploy/gatherkeys.py
@@ -29,47 +29,47 @@ def fetch_file(args, frompath, topath, hosts):
     return False
 
 def gatherkeys(args):
-     ret = 0
+    ret = 0
 
-     # client.admin
-     r = fetch_file(
-          args=args,
-          frompath='/etc/ceph/{cluster}.client.admin.keyring'.format(
-               cluster=args.cluster),
-          topath='{cluster}.client.admin.keyring'.format(
-               cluster=args.cluster),
-          hosts=args.mon,
-          )
-     if not r:
-          ret = 1
+    # client.admin
+    r = fetch_file(
+        args=args,
+        frompath='/etc/ceph/{cluster}.client.admin.keyring'.format(
+            cluster=args.cluster),
+        topath='{cluster}.client.admin.keyring'.format(
+            cluster=args.cluster),
+        hosts=args.mon,
+        )
+    if not r:
+        ret = 1
 
-     # mon.
-     fetch_file(
-          args=args,
-          frompath='/var/lib/ceph/mon/%s-{hostname}/keyring' % args.cluster,
-          topath='{cluster}.mon.keyring'.format(
-               cluster=args.cluster),
-          hosts=args.mon,
-          )
-     if not r:
-          ret = 1
+    # mon.
+    fetch_file(
+        args=args,
+        frompath='/var/lib/ceph/mon/%s-{hostname}/keyring' % args.cluster,
+        topath='{cluster}.mon.keyring'.format(
+             cluster=args.cluster),
+        hosts=args.mon,
+        )
+    if not r:
+        ret = 1
 
-     # bootstrap
-     for what in ['osd', 'mds']:
-          r = fetch_file(
-               args=args,
-               frompath='/var/lib/ceph/bootstrap-{what}/{cluster}.keyring'.format(
-                    cluster=args.cluster,
-                    what=what),
-               topath='{cluster}.bootstrap-{what}.keyring'.format(
-                    cluster=args.cluster,
-                    what=what),
-               hosts=args.mon,
-               )
-          if not r:
-               ret = 1
+    # bootstrap
+    for what in ['osd', 'mds']:
+        r = fetch_file(
+            args=args,
+            frompath='/var/lib/ceph/bootstrap-{what}/{cluster}.keyring'.format(
+                cluster=args.cluster,
+                what=what),
+            topath='{cluster}.bootstrap-{what}.keyring'.format(
+                cluster=args.cluster,
+                what=what),
+            hosts=args.mon,
+            )
+        if not r:
+            ret = 1
 
-     return ret
+    return ret
 
 @priority(40)
 def make(parser):
