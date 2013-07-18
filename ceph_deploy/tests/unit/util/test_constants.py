0d14d838d9409a8c0332b41eb31b2d3c9c7223d6
Alfredo Deza <alfredo@deza.pe>
2013-07-18 10:30:15 -0400

    add the tmp path with tests

--- ceph_deploy/tests/unit/util/test_constants.py
+++ ceph_deploy/tests/unit/util/test_constants.py
@@ -10,3 +10,7 @@ class TestPaths(object):
     def test_mds_path(self):
         assert constants.mds_path.startswith('/')
         assert constants.mds_path.endswith('/mds')
+
+    def test_tmp_path(self):
+        assert constants.tmp_path.startswith('/')
+        assert constants.tmp_path.endswith('/tmp')
