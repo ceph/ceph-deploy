31d1689aad57929b1cf65c362d39c804b870335b
Alfredo Deza <alfredo@deza.pe>
2013-07-24 13:21:03 -0400

    more tests for get_distro

--- ceph_deploy/tests/unit/hosts/test_hosts.py
+++ ceph_deploy/tests/unit/hosts/test_hosts.py
@@ -1,3 +1,6 @@
+from pytest import raises
+
+from ceph_deploy import exc
 from ceph_deploy import hosts
 
 
@@ -18,3 +21,31 @@ class TestNormalized(object):
     def test_get_redhat(self):
         result = hosts._normalized_distro_name('RedHatEnterpriseLinux')
         assert result == 'redhat'
+
+
+class TestGetDistro(object):
+
+    def test_get_debian(self):
+        result = hosts._get_distro('Debian')
+        assert result.__name__.endswith('debian')
+
+    def test_get_ubuntu(self):
+        # Ubuntu imports debian stuff
+        result = hosts._get_distro('Ubuntu')
+        assert result.__name__.endswith('debian')
+
+    def test_get_centos(self):
+        result = hosts._get_distro('CentOS')
+        assert result.__name__.endswith('centos')
+
+    def test_get_scientific(self):
+        result = hosts._get_distro('Scientific')
+        assert result.__name__.endswith('centos')
+
+    def test_get_redhat(self):
+        result = hosts._get_distro('RedHat')
+        assert result.__name__.endswith('centos')
+
+    def test_get_uknown(self):
+        with raises(exc.UnsupportedPlatform):
+            hosts._get_distro('Solaris')
