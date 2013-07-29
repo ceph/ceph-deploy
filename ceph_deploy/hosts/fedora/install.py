fc7d46f039fd3462784d5d4616caef3a8ace7c11
Alfredo Deza <alfredo@deza.pe>
2013-07-29 15:59:25 -0400

    fedora adapated to work with the new wrappers
    Signed-off-by: Alfredo Deza <alfredo@deza.pe>

--- ceph_deploy/hosts/fedora/install.py
+++ ceph_deploy/hosts/fedora/install.py
@@ -1,15 +1,19 @@
+from ceph_deploy.util.wrappers import check_call
+from ceph_deploy.util.context import remote
 
 
-def install(release, codename, version_kind, version):
-    import platform
-    import subprocess
+def install(distro, logger, release, version_kind, version):
+    release = distro.release
+    machine = distro.sudo_conn.modules.platform.machine()
 
     if version_kind in ['stable', 'testing']:
         key = 'release'
     else:
         key = 'autobuild'
 
-    subprocess.check_call(
+    check_call(
+        distro.sudo_conn,
+        logger,
         args='su -c \'rpm --import "https://ceph.com/git/?p=ceph.git;a=blob_plain;f=keys/{key}.asc"\''.format(key=key),
         shell=True,
         )
@@ -26,11 +30,13 @@ def install(release, codename, version_kind, version):
     elif version_kind == 'dev':
         url = 'http://gitbuilder.ceph.com/ceph-rpm-fc{release}-{machine}-basic/ref/{version}/'.format(
             release=release.split(".", 1)[0],
-            machine=platform.machine(),
+            machine=machine,
             version=version,
             )
 
-    subprocess.check_call(
+    check_call(
+        distro.sudo_conn,
+        logger,
         args=[
             'rpm',
             '-Uvh',
@@ -44,7 +50,9 @@ def install(release, codename, version_kind, version):
             ]
         )
 
-    subprocess.check_call(
+    check_call(
+        distro.sudo_conn,
+        logger,
         args=[
             'yum',
             '-y',
