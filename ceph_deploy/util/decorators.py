31ea44f3eb594480a9b4a8d23c60d4de3bf3b024
Alfredo Deza <alfredo@deza.pe>
2013-07-29 10:43:08 -0400

    remote_compile saves the exc and does not require the logger
    Signed-off-by: Alfredo Deza <alfredo@deza.pe>

--- ceph_deploy/util/decorators.py
+++ ceph_deploy/util/decorators.py
@@ -1,20 +1,21 @@
 
 
-def remote_compile(client, fn, logger):
+def remote_compile(client, fn):
     def outer(fn):
         from functools import wraps
         @wraps(fn)
         def inner(*args, **kwargs):
             class RemoteException(Exception):
 
-                def __init__(self, remote_traceback):
+                def __init__(self, remote_traceback, err):
+                    self.err = err
                     self.remote_traceback = remote_traceback
 
             try:
                 fn(*args, **kwargs)
-            except:
+            except Exception as err:
                 import traceback
                 remote_trace = traceback.format_exc()
-                raise RemoteException(remote_trace.split('\n'))
+                raise RemoteException(remote_trace.split('\n'), err)
         return inner
     return client.compile(outer)(client.compile(fn))
