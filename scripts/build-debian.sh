a74a0672ca89fd737b204a42422f01b8136eb43f
Gary Lowell <gary.lowell@inktank.com>
2013-08-12 15:22:48 -0700

    build-debian.sh:  Update for release builds.
    Add release key, and backport tags.
    
    Signed-off-by: Gary Lowell  <gary.lowell@inktank.com>

--- scripts/build-debian.sh
+++ scripts/build-debian.sh
@@ -8,6 +8,13 @@ COMPONENT=main
 KEYID=03C3951A  # Autobuild keyid
 DEB_DIST="sid wheezy squeeze quantal precise oneiric natty raring"
 DEB_BUILD=$(lsb_release -s -c)
+RELEASE=0
+
+if [ X"$1" = X"--release" ] ; then
+    echo "Release Build"
+    RELEASE=1
+    KEYID=17ED316D
+fi
 
 if [ ! -d debian ] ; then
     echo "Are we in the right directory"
@@ -20,6 +27,18 @@ else
     echo "Package signing key (${KEYID}) not found"
     echo "Have you set \$GNUPGHOME ? "
     exit 3
+fi
+
+# Clean up any leftover builds
+rm -f ../ceph-deploy*.dsc ../ceph-deploy*.changes ../ceph-deploy*.deb ../ceph-deploy.tgz
+rm -rf ./debian-repo
+
+# Apply backport tag if release build
+if [ $RELEASE -eq 1 ] ; then 
+    DEB_VERSION=$(dpkg-parsechangelog | sed -rne 's,^Version: (.*),\1, p')
+    BP_VERSION=${DEB_VERSION}${BPTAG}
+    DEBEMAIL="gary.lowell@inktank.com" dch -D $DIST --force-distribution -b -v "$BP_VERSION" "$comment"
+    dpkd-source -b .
 fi
 
 # Build Package
