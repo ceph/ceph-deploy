bdfdd6c949d1e64ae9f312e114e6f03b88963a67
Alfredo Deza <alfredo.deza@inktank.com>
2013-10-11 12:58:11 -0400

    install fully ported to remoto
    Signed-off-by: Alfredo Deza <alfredo.deza@inktank.com>

--- ceph_deploy/install.py
+++ ceph_deploy/install.py
@@ -2,150 +2,24 @@ import argparse
 import logging
 from distutils.util import strtobool
 
-from . import exc
-from . import lsb, hosts
+from . import hosts
 from .cliutil import priority
-from .sudo_pushy import get_transport
-from .util.decorators import remote_compile
+from .lib.remoto import process
+
 
 LOG = logging.getLogger(__name__)
 
-def check_ceph_installed():
+
+def ceph_is_installed(conn):
     """
     Check if the ceph packages are installed by looking for the
     presence of the ceph command.
     """
-    import subprocess
-
-    args = [ 'which', 'ceph', ]
-    process = subprocess.Popen(
-        args=args,
-        )
-    lsb_release_path, _ = process.communicate()
-    return process.wait()
-
-
-def uninstall_suse(arg_purge=False):
-    import subprocess
-
-    packages = [
-        'ceph',
-        'libcephfs1',
-        'librados2',
-        'librbd1',
-        ]
-    args = [
-        'zypper',
-        '--non-interactive',
-        '--quiet',
-        'remove',
-        ]
-
-    args.extend(packages)
-    subprocess.check_call(args=args)
-
-def uninstall_debian(arg_purge=False):
-    import subprocess
-
-    packages = [
-        'ceph',
-        'ceph-mds',
-        'ceph-common',
-        'ceph-fs-common',
-        ]
-    args = [
-        'apt-get',
-        '-q',
-        'remove',
-        '-f',
-        '-y',
-        '--force-yes',
-        ]
-    if arg_purge:
-        args.append('--purge')
-    args.append('--')
-    args.extend(packages)
-    subprocess.check_call(args=args)
-
-
-def uninstall_fedora(arg_purge=False):
-    import subprocess
-
-    packages = [
-        'ceph',
-        ]
-    args = [
-        'yum',
-        '-q',
-        '-y',
-        'remove',
-        ]
-
-    args.extend(packages)
-    subprocess.check_call(args=args)
-
-
-def uninstall_centos(arg_purge=False):
-    import subprocess
-
-    packages = [
-        'ceph',
-        ]
-    args = [
-        'yum',
-        '-q',
-        '-y',
-        'remove',
-        ]
-
-    args.extend(packages)
-    subprocess.check_call(args=args)
-
-def uninstall_debian(arg_purge=False):
-    import subprocess
-
-    packages = [
-        'ceph',
-        'ceph-mds',
-        'ceph-common',
-        'ceph-fs-common',
-        ]
-    args = [
-        'apt-get',
-        '-q',
-        'remove',
-        '-f',
-        '-y',
-        '--force-yes',
-        ]
-    if arg_purge:
-        args.append('--purge')
-    args.append('--')
-    args.extend(packages)
-    subprocess.check_call(args=args)
-
-
-def purge_data_any():
-    import subprocess
-    import os.path
-
-    subprocess.call(args=[
-            'rm', '-rf', '--one-file-system', '--', '/var/lib/ceph',
-            ])
-    if os.path.exists('/var/lib/ceph'):
-        subprocess.check_call(args=[
-                'find', '/var/lib/ceph',
-                '-mindepth', '1',
-                '-maxdepth', '2',
-                '-type', 'd',
-                '-exec', 'umount', '{}', ';',
-                ])
-        subprocess.check_call(args=[
-                'rm', '-rf', '--one-file-system', '--', '/var/lib/ceph',
-                ])
-    subprocess.check_call(args=[
-            'rm', '-rf', '--one-file-system', '--', '/etc/ceph/*',
-            ])
+    stdout, stderr, return_code = process.check(
+        conn,
+        ['which', 'ceph'],
+    )
+    return return_code
 
 
 def install(args):
@@ -183,24 +57,12 @@ def uninstall(args):
         LOG.debug('Detecting platform for host %s ...', hostname)
 
         # TODO username
-        sudo = args.pushy(get_transport(hostname))
-        (distro, release, codename) = lsb.get_lsb_release(sudo)
-        LOG.debug('Distro %s codename %s', distro, codename)
-
-        if (distro == 'Debian' or distro == 'Ubuntu'):
-            uninstall_r = sudo.compile(uninstall_debian)
-        elif distro == 'CentOS' or distro == 'Scientific' or distro.startswith('RedHat'):
-            uninstall_r = sudo.compile(uninstall_centos)
-        elif distro == 'Fedora':
-            uninstall_r = sudo.compile(uninstall_fedora)
-        elif (distro == 'SUSE LINUX'):
-            uninstall_r = sudo.compile(uninstall_suse)
-        else:
-            raise exc.UnsupportedPlatform(distro=distro, codename=codename)
-
-        LOG.debug('Uninstalling on host %s ...', hostname)
-        uninstall_r()
-        sudo.close()
+        distro = hosts.get(hostname)
+        LOG.info('Distro info: %s %s %s', distro.name, distro.release, distro.codename)
+        rlogger = logging.getLogger(hostname)
+        rlogger.info('uninstalling ceph on %s' % hostname)
+        distro.uninstall(distro.conn)
+        distro.conn.exit()
 
 
 def purge(args):
@@ -214,24 +76,12 @@ def purge(args):
         LOG.debug('Detecting platform for host %s ...', hostname)
 
         # TODO username
-        sudo = args.pushy(get_transport(hostname))
-        (distro, release, codename) = lsb.get_lsb_release(sudo)
-        LOG.debug('Distro %s codename %s', distro, codename)
-
-        if distro == 'Debian' or distro == 'Ubuntu':
-            purge_r = sudo.compile(uninstall_debian)
-        elif distro == 'CentOS' or distro == 'Scientific' or distro.startswith('RedHat'):
-            purge_r = sudo.compile(uninstall_centos)
-        elif distro == 'Fedora':
-            purge_r = sudo.compile(uninstall_fedora)
-        elif (distro == 'SUSE LINUX'):
-            purge_r = sudo.compile(uninstall_suse)
-        else:
-            raise exc.UnsupportedPlatform(distro=distro, codename=codename)
-
-        LOG.debug('Purging host %s ...', hostname)
-        purge_r(arg_purge=True)
-        sudo.close()
+        distro = hosts.get(hostname)
+        LOG.info('Distro info: %s %s %s', distro.name, distro.release, distro.codename)
+        rlogger = logging.getLogger(hostname)
+        rlogger.info('purging host ... %s' % hostname)
+        distro.uninstall(distro.conn, purge=True)
+        distro.conn.exit()
 
 
 def purge_data(args):
@@ -241,29 +91,58 @@ def purge_data(args):
         ' '.join(args.host),
         )
 
-    installed_hosts=[]
+    installed_hosts = []
     for hostname in args.host:
-        sudo = args.pushy(get_transport(hostname))
-        check_ceph_installed_r  = sudo.compile(check_ceph_installed)
-        status = check_ceph_installed_r()
-        if status == 0:
+        distro = hosts.get(hostname)
+        if ceph_is_installed(distro.conn):
             installed_hosts.append(hostname)
-        sudo.close()
+        distro.conn.exit()
 
     if installed_hosts:
         print "ceph is still installed on: ", installed_hosts
-        answer=raw_input("Continue (y/n)")
+        answer = raw_input("Continue (y/n)")
         if not strtobool(answer):
             return
 
     for hostname in args.host:
         # TODO username
-        sudo = args.pushy(get_transport(hostname))
+        distro = hosts.get(hostname)
+        LOG.info('Distro info: %s %s %s', distro.name, distro.release, distro.codename)
+        rlogger = logging.getLogger(hostname)
+        rlogger.info('purging data on %s' % hostname)
 
-        LOG.debug('Purging data from host %s ...', hostname)
-        purge_data_any_r = sudo.compile(purge_data_any)
-        purge_data_any_r()
-        sudo.close()
+        process.run(
+            distro.conn,
+            [
+                'rm', '-rf', '--one-file-system', '--', '/var/lib/ceph',
+            ]
+        )
+        if distro.conn.remote_module.path_exists('/var/lib/ceph'):
+            process.run(
+                distro.conn,
+                [
+                    'find', '/var/lib/ceph',
+                    '-mindepth', '1',
+                    '-maxdepth', '2',
+                    '-type', 'd',
+                    '-exec', 'umount', '{}', ';',
+                ]
+            )
+            process.run(
+                distro.conn,
+                [
+                    'rm', '-rf', '--one-file-system', '--', '/var/lib/ceph',
+                ]
+            )
+
+        process.run(
+            distro.conn,
+            [
+                'rm', '-rf', '--one-file-system', '--', '/etc/ceph/*',
+            ]
+        )
+
+        distro.conn.exit()
 
 
 class StoreVersion(argparse.Action):
@@ -368,6 +247,7 @@ def make_uninstall(parser):
     parser.set_defaults(
         func=uninstall,
         )
+
 
 @priority(80)
 def make_purge(parser):
