850fc2eca7cce41360fcfa63a52b141055e99123
Alfredo Deza <alfredo.deza@inktank.com>
2013-10-14 15:30:25 -0400

    remove remote_compile decorator
    Signed-off-by: Alfredo Deza <alfredo.deza@inktank.com>

--- ceph_deploy/util/decorators.py
+++ ceph_deploy/util/decorators.py
@@ -3,27 +3,6 @@ import sys
 from functools import wraps
 
 
-def remote_compile(client, fn):
-    def outer(fn):
-        from functools import wraps
-        @wraps(fn)
-        def inner(*args, **kwargs):
-            class RemoteException(Exception):
-
-                def __init__(self, remote_traceback, err):
-                    self.err = err
-                    self.remote_traceback = remote_traceback
-
-            try:
-                return fn(*args, **kwargs)
-            except Exception as err:
-                import traceback
-                remote_trace = traceback.format_exc()
-                raise RemoteException(remote_trace.split('\n'), err)
-        return inner
-    return client.compile(outer)(client.compile(fn))
-
-
 def catches(catch=None, handler=None, exit=True):
     """
     Very simple decorator that tries any of the exception(s) passed in as
