d204607216bce10153c1260ac8e2d70a1644bf7a
Alfredo Deza <alfredo.deza@inktank.com>
2013-10-21 10:26:02 -0400

    use the ceph theme
    Signed-off-by: Alfredo Deza <alfredo.deza@inktank.com>

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -18,6 +18,7 @@ import os
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
+sys.path.append(os.path.abspath('_themes'))
 sys.path.insert(0, os.path.abspath('..'))
 import ceph_deploy
 
@@ -102,7 +103,7 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'default'
+html_theme = 'ceph'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -110,7 +111,7 @@ html_theme = 'default'
 #html_theme_options = {}
 
 # Add any paths that contain custom themes here, relative to this directory.
-#html_theme_path = []
+html_theme_path = ['_themes']
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
