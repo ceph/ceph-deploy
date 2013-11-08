4499b4118dce3594face72ffeba17a4f7474a8a6
Alfredo Deza <alfredo.deza@inktank.com>
2013-11-08 10:25:43 -0500

    create a local connection helper
    Signed-off-by: Alfredo Deza <alfredo.deza@inktank.com>

--- ceph_deploy/connection.py
+++ ceph_deploy/connection.py
@@ -1,4 +1,5 @@
 import getpass
+import socket
 from ceph_deploy.lib.remoto import Connection
 
 
@@ -29,6 +30,20 @@ def get_connection(hostname, username, logger, threads=5, use_sudo=None):
         msg = "connecting to host: %s " % hostname
         errors = "resulted in errors: %s %s" % (error.__class__.__name__, error)
         raise RuntimeError(msg + errors)
+
+
+def get_local_connection(logger, use_sudo=False):
+    """
+    Helper for local connections that are sometimes needed to operate
+    on local hosts
+    """
+    return get_connection(
+        socket.gethostname(),  # cannot rely on 'localhost' here
+        None,
+        logger=logger,
+        threads=1,
+        use_sudo=use_sudo
+    )
 
 
 def needs_sudo():
