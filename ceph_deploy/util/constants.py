d33d275eeccc5401af59f7802c0e8e2cf1095ac7
Alfredo Deza <alfredo.deza@inktank.com>
2013-12-06 10:44:20 -0500

    create a base run path in constants
    Signed-off-by: Alfredo Deza <alfredo.deza@inktank.com>

--- ceph_deploy/util/constants.py
+++ ceph_deploy/util/constants.py
@@ -3,6 +3,9 @@ from os.path import join
 # Base Path for ceph
 base_path = '/var/lib/ceph'
 
+# Base run Path
+base_run_path = '/var/run/ceph'
+
 tmp_path = join(base_path, 'tmp')
 
 mon_path = join(base_path, 'mon')
