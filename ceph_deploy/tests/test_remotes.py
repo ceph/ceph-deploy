201c5d0649ffe2fa404cc1a4f11d002bd09a6eef
Andrew Woodward <awoodward@mirantis.com>
2014-01-08 16:24:37 -0800

    Add test to properly catch debian/sid strings from version
    strings for wheezy/sid and jessie/sid aren't caught by the version number
     test already implmented.
    
    Adds test to check if codename is still empty and version contains a slash
     if so, and minor is sid, we set codename to sid. Otherwise we set codename
     to major.
    
    Adds test cases for platform_information to test expected usage patterns
     and covers some common os strings.
    
    Futher assessment may then made to see if we should then refuse
     jessie or sid
    
    Signed-off-by: Andrew Woodward <awoodward@mirantis.com>

--- ceph_deploy/tests/test_remotes.py
+++ ceph_deploy/tests/test_remotes.py
@@ -1,6 +1,6 @@
 from mock import patch
 from ceph_deploy.hosts import remotes
-
+from ceph_deploy.hosts.remotes import platform_information
 
 class FakeExists(object):
 
@@ -30,3 +30,56 @@ class TestWhich(object):
             path = remotes.which('ls')
         assert path is None
 
+class TestPlatformInformation(object):
+    """ tests various inputs that remotes.platform_information handles
+
+    you can test your OS string by comparing the results with the output from:
+      python -c "import platform; print platform.linux_distribution()"
+    """
+
+    def setup(self):
+        pass
+
+    def test_handles_deb_version_num(self):
+        def fake_distro(): return ('debian', '8.4', '')
+        distro, release, codename = platform_information(fake_distro)
+        assert distro == 'debian'
+        assert release == '8.4'
+        assert codename == 'jessie'
+
+    def test_handles_deb_version_slash(self):
+        def fake_distro(): return ('debian', 'wheezy/something', '')
+        distro, release, codename = platform_information(fake_distro)
+        assert distro == 'debian'
+        assert release == 'wheezy/something'
+        assert codename == 'wheezy'
+
+    def test_handles_deb_version_slash_sid(self):
+        def fake_distro(): return ('debian', 'jessie/sid', '')
+        distro, release, codename = platform_information(fake_distro)
+        assert distro == 'debian'
+        assert release == 'jessie/sid'
+        assert codename == 'sid'
+
+    def test_handles_no_codename(self):
+        def fake_distro(): return ('SlaOS', '99.999', '')
+        distro, release, codename = platform_information(fake_distro)
+        assert distro == 'SlaOS'
+        assert release == '99.999'
+        assert codename == ''
+
+    # Normal distro strings
+    def test_hanles_centos_64(self):
+        def fake_distro(): return ('CentOS', '6.4', 'Final')
+        distro, release, codename = platform_information(fake_distro)
+        assert distro == 'CentOS'
+        assert release == '6.4'
+        assert codename == 'Final'
+
+
+    def test_handles_ubuntu_percise(self):
+        def fake_distro(): return ('Ubuntu', '12.04', 'precise')
+        distro, release, codename = platform_information(fake_distro)
+        assert distro == 'Ubuntu'
+        assert release == '12.04'
+        assert codename == 'precise'
