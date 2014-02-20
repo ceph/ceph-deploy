e4db2fd0f5e8a08b2d51530fc65d3c621848d8c5
Alfredo Deza <alfredo.deza@inktank.com>
2014-02-20 15:49:35 -0500

    import mon_add from common
    Signed-off-by: Alfredo Deza <alfredo.deza@inktank.com>

--- ceph_deploy/hosts/centos/mon/__init__.py
+++ ceph_deploy/hosts/centos/mon/__init__.py
@@ -1,2 +1,2 @@
+from ceph_deploy.hosts.common import mon_add as add
 from create import create
-from add import add
--- ceph_deploy/hosts/fedora/mon/__init__.py
+++ ceph_deploy/hosts/fedora/mon/__init__.py
@@ -1,2 +1,2 @@
-from add import add
+from ceph_deploy.hosts.common import mon_add as add
 from create import create
