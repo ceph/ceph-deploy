c3ee8cc7fca82eb1743c8809dc0a91a7af1f0700
Alfredo Deza <alfredo.deza@inktank.com>
2014-02-20 14:28:24 -0500

    create the add.py file for suse
    Signed-off-by: Alfredo Deza <alfredo.deza@inktank.com>

--- ceph_deploy/hosts/suse/mon/add.py
+++ ceph_deploy/hosts/suse/mon/add.py
@@ -2,9 +2,9 @@ from ceph_deploy.hosts import common
 from ceph_deploy.lib.remoto import process
 
 
-def create(distro, args, monitor_keyring):
+def add(distro, args, monitor_keyring):
     hostname = distro.conn.remote_module.shortname()
-    common.mon_create(distro, args, monitor_keyring, hostname)
+    common.mon_add(distro, args, monitor_keyring, hostname)
 
     process.run(
         distro.conn,
