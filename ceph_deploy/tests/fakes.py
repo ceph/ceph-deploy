86b089d3848d6a9097de962464f8a0d35cc8df9f
Alfredo Deza <alfredo.deza@inktank.com>
2014-03-04 13:34:15 -0500

    add mock imports to fakes
    Signed-off-by: Alfredo Deza <alfredo.deza@inktank.com>

--- ceph_deploy/tests/fakes.py
+++ ceph_deploy/tests/fakes.py
@@ -1,5 +1,26 @@
+from mock import MagicMock
 
 
 def fake_getaddrinfo(*a, **kw):
     return_host = kw.get('return_host', 'host1')
     return [[0,0,0,0, return_host]]
+
+
+def mock_open(mock=None, data=None):
+    """
+    Fake the behavior of `open` when used as a context manager
+    """
+    if mock is None:
+        mock = MagicMock(spec=file)
+
+    handle = MagicMock(spec=file)
+    handle.write.return_value = None
+    if data is None:
+        handle.__enter__.return_value = handle
+    else:
+        handle.__enter__.return_value = data
+    mock.return_value = handle
+    return mock
+
+
+
