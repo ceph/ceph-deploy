6f4116ad785d9d8773c5745221fc21e31c7fbe06
Alfredo Deza <alfredo.deza@inktank.com>
2014-06-24 11:52:38 -0400

    support remoto imports for both vendored and global installs
    Signed-off-by: Alfredo Deza <alfredo.deza@inktank.com>

--- ceph_deploy/lib/__init__.py
+++ ceph_deploy/lib/__init__.py
@@ -5,6 +5,23 @@ imports.
 
 Do **not** add anything to this module that does not represent a vendorized
 library.
+
+Vendored libraries should go into the ``vendor`` directory and imported from
+there. This is so we allow libraries that are installed normally to be imported
+if the vendored module is not available.
+
+The import dance here is done so that all other imports throught ceph-deploy
+are kept the same regardless of where the module comes from.
+
+The expected way to import remoto would look like this::
+
+    from ceph_deploy.lib import remoto
+
 """
 
-import remoto
+try:
+    # vendored
+    from vendor import remoto
+except ImportError:
+    # normally installed
+    import remoto  # noqa
