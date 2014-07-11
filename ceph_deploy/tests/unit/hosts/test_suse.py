d74906ce5110ebc70b09c6c2691a7531ba076c94
Owen Synge <osynge@suse.com>
2014-07-11 18:21:16 +0200

    adds 4 very basic tests for init type

---
+++ ceph_deploy/tests/unit/hosts/test_suse.py
@@ -0,0 +1,25 @@
+from ceph_deploy.hosts import suse 
+
+class TestSuseInit(object):
+    def setup(self):
+        self.host = suse
+
+    def test_choose_init_default(self):
+        self.host.release = '11'
+        init_type = self.host.choose_init()
+        assert ( init_type == "sysvinit")
+        
+    def test_choose_init_SLE_11(self):
+        self.host.release = '11'
+        init_type = self.host.choose_init()
+        assert ( init_type == "sysvinit")
+
+    def test_choose_init_SLE_12(self):
+        self.host.release = '12'
+        init_type = self.host.choose_init()
+        assert ( init_type == "systemd")
+
+    def test_choose_init_openSUSE_13.1(self):
+        self.host.release = '13.1'
+        init_type = self.host.choose_init()
+        assert ( init_type == "systemd")
