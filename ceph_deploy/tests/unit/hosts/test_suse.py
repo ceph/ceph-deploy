40b89abafd29447b07cb4d9995ade0872b70b4bc
Owen Synge <osynge@suse.com>
2014-07-11 18:43:47 +0200

    don't need the parenthesis

--- ceph_deploy/tests/unit/hosts/test_suse.py
+++ ceph_deploy/tests/unit/hosts/test_suse.py
@@ -7,19 +7,19 @@ class TestSuseInit(object):
     def test_choose_init_default(self):
         self.host.release = None
         init_type = self.host.choose_init()
-        assert ( init_type == "sysvinit")
+        assert init_type == "sysvinit"
         
     def test_choose_init_SLE_11(self):
         self.host.release = '11'
         init_type = self.host.choose_init()
-        assert ( init_type == "sysvinit")
+        assert init_type == "sysvinit"
 
     def test_choose_init_SLE_12(self):
         self.host.release = '12'
         init_type = self.host.choose_init()
-        assert ( init_type == "systemd")
+        assert init_type == "systemd"
 
     def test_choose_init_openSUSE_13_1(self):
         self.host.release = '13.1'
         init_type = self.host.choose_init()
-        assert ( init_type == "systemd")
+        assert init_type == "systemd"
