79d388f591fb8fe9af3b391d3ec3b17634647257
Tolbkni Kao <tolbkni@gmail.com>
2014-07-23 21:32:15 +0800

    Check if ssh command prints "Host key verification failed." error message
    "can_connect_passwordless" func will return false if ssh with an exit status
    of 255 and a "Host key verification failed." message

--- ceph_deploy/util/ssh.py
+++ ceph_deploy/util/ssh.py
@@ -9,7 +9,7 @@ def can_connect_passwordless(hostname):
     host using the ``BatchMode`` option to prevent a password prompt.
 
     That attempt will error with an exit status of 255 and a ``Permission
-    denied`` message.
+    denied`` message or a``Host key verification failed`` message.
     """
     # Ensure we are not doing this for local hosts
     if not remoto.connection.needs_ssh(hostname):
@@ -20,10 +20,11 @@ def can_connect_passwordless(hostname):
         # Check to see if we can login, disabling password prompts
         command = ['ssh', '-CT', '-o', 'BatchMode=yes', hostname]
         out, err, retval = remoto.process.check(conn, command, stop_on_error=False)
-        expected_error = 'Permission denied '
+        permission_denied_error = 'Permission denied '
+        host_key_verify_error = 'Host key verification failed.'
         has_key_error = False
         for line in err:
-            if expected_error in line:
+            if permission_denied_error in line or host_key_verify_error in line:
                 has_key_error = True
 
         if retval == 255 and has_key_error:
