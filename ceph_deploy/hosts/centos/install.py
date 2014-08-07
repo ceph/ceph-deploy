d737a664bbb4052d1a563254ab8d3a165a87f707
Tom Walsh <tom.walsh@expresshosting.net>
2014-08-07 18:36:00 -0500

    Added checks to the CentOS install scripts to properly detect CentOS 7 and set the correct repo information.
    Added tests to confirm that the changes to CentOS detection are working correctly.
    
    Signed-off-by: Tom Walsh <tom.walsh@expresshosting.net>

--- ceph_deploy/hosts/centos/install.py
+++ ceph_deploy/hosts/centos/install.py
@@ -6,6 +6,8 @@ def rpm_dist(distro):
     # start using the el7 prefix now that rhel7 exists.
     if distro.normalized_name == 'redhat' and distro.release.startswith('7'):
         return 'el7'
+    elif distro.normalized_name == 'centos' and distro.release.startswith('7'):
+        return 'el7'
     return 'el6'
 
 
@@ -30,6 +32,11 @@ def repository_url_part(distro):
             return 'rhel6'
         elif distro.release.startswith('7'):
             return 'rhel7'
+
+    if distro.normalized_name == 'centos':
+        if distro.release.startswith('7'):
+            return 'el7'
+
     return 'el6'
 
 
