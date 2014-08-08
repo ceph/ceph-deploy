31f928a892b62f40260a85ccda7ccc1b1fc0254e
Tom Walsh <tom.walsh@expresshosting.net>
2014-08-07 19:25:00 -0500

    Fine tuned the tests to catch some more corner cases.
    Adjusted the logic in install.py to match what was already there.
    
    Signed-off-by: Tom Walsh <tom.walsh@expresshosting.net>

--- ceph_deploy/tests/unit/hosts/test_centos.py
+++ ceph_deploy/tests/unit/hosts/test_centos.py
@@ -38,10 +38,10 @@ class TestCentosVersionDetection(object):
         self.distro.release = '7.0'
         assert centos.rpm_dist(self.distro) == 'el7'
 
-"""
-    Adding specific tests here to detect more than just RedHat (CentOS)
-
-    """
+    def test_url_fallsback_to_el6_centos(self):
+        self.distro.normalized_name = 'centos'
+        self.distro.release = ''
+        assert centos.repository_url_part(self.distro) == 'el6'
 
     def test_url_detects_el5(self):
         self.distro.normalized_name = 'centos'
@@ -50,6 +50,7 @@ class TestCentosVersionDetection(object):
 
     def test_url_detects_el6(self):
         self.distro.normalized_name = 'centos'
+        self.distro.release = '6.0'
         assert centos.repository_url_part(self.distro) == 'el6'
 
     def test_url_detects_el7(self):
