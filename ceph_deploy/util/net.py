512bb578c5c8f6d0af6de8c5d0548a933a4f969a
Alfredo Deza <alfredo.deza@inktank.com>
2014-08-20 12:56:12 -0400

    log the fact that we were (or were not) able to match an IP to a subnet
    Signed-off-by: Alfredo Deza <alfredo.deza@inktank.com>

--- ceph_deploy/util/net.py
+++ ceph_deploy/util/net.py
@@ -1,8 +1,16 @@
 from ceph_deploy import exc
+import logging
 import socket
 
 
-def get_nonlocal_ip(host):
+LOG = logging.getLogger(__name__)
+
+
+# TODO: at some point, it might be way more accurate to do this in the actual
+# host where we need to get IPs from. SaltStack does this by calling `ip` and
+# parsing the output, which is probably the one true way of dealing with it.
+
+def get_nonlocal_ip(host, subnet=None):
     """
     Search result of getaddrinfo() for a non-localhost-net address
     """
@@ -13,7 +21,22 @@ def get_nonlocal_ip(host):
     for ai in ailist:
         # an ai is a 5-tuple; the last element is (ip, port)
         ip = ai[4][0]
+        if subnet and ip_in_subnet(ip, subnet):
+            LOG.info('found ip (%s) for host (%s) to be in cluster subnet (%s)' % (
+                ip,
+                host,
+                subnet,)
+            )
+
+            return ip
+
         if not ip.startswith('127.'):
+            if subnet:
+                LOG.warning('could not match ip (%s) for host (%s) for cluster subnet (%s)' % (
+                    ip,
+                    host,
+                    subnet,)
+                )
             return ip
     raise exc.UnableToResolveError(host)
 
