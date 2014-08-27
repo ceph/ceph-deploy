0f78f3dd2e4110bd6131538c13e398c89b76a32b
Alfredo Deza <alfredo.deza@inktank.com>
2014-08-27 15:21:06 -0400

    add tests to ensure we do the right thing with per subnet validation
    Signed-off-by: Alfredo Deza <alfredo.deza@inktank.com>

---
+++ ceph_deploy/tests/util.py
@@ -0,0 +1,21 @@
+
+
+def generate_ips(start_ip, end_ip):
+    start = list(map(int, start_ip.split(".")))
+    end = list(map(int, end_ip.split(".")))
+    temp = start
+    ip_range = []
+
+    ip_range.append(start_ip)
+    while temp != end:
+        start[3] += 1
+        for i in (3, 2, 1):
+            if temp[i] == 256:
+                temp[i] = 0
+                temp[i-1] += 1
+        ip_range.append(".".join(map(str, temp)))
+
+    return ip_range
+
+
+
