2f05473059c41725c6ddb5fbc0bd36156f93245f
Alfredo Deza <alfredo.deza@inktank.com>
2014-09-23 12:56:17 -0400

    add tests for the grep utilities in remotes
    Signed-off-by: Alfredo Deza <alfredo.deza@inktank.com>

---
+++ ceph_deploy/tests/unit/hosts/test_remotes.py
@@ -0,0 +1,16 @@
+from cStringIO import StringIO
+
+from ceph_deploy.hosts import remotes
+
+
+class TestObjectGrep(object):
+
+    def setup(self):
+        self.file_object = StringIO('foo\n')
+        self.file_object.seek(0)
+
+    def test_finds_term(self):
+        assert remotes.object_grep('foo', self.file_object)
+
+    def test_does_not_find_anything(self):
+        assert remotes.object_grep('bar', self.file_object) is False
