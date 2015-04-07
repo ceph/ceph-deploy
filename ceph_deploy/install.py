1b1e3180b9792a947410a46371cf6dcd3058a39d
Travis Rhoden <trhoden@redhat.com>
2015-04-07 17:12:02 -0400

    change default release to Hammer
    Signed-off-by: Travis Rhoden <trhoden@redhat.com>

--- ceph_deploy/install.py
+++ ceph_deploy/install.py
@@ -16,7 +16,7 @@ def sanitize_args(args):
     not well suited for.
     """
     if args.release is None:
-        args.release = 'giant'
+        args.release = 'hammer'
         args.default_release = True
 
     # XXX This whole dance is because --stable is getting deprecated
