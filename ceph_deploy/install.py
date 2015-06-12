91caaebd5f2b8011bef45c40456917fcaa6f0a97
David Disseldorp <ddiss@suse.de>
2015-06-12 18:09:42 +0200

    install: add --iscsi parameter for targetcli installation
    With support for clustered RBD/LIO iSCSI targets in development, this
    change adds an --iscsi parameter to "ceph-deploy install".
    
    For host back-ends that support component granular installations
    (currently only rhel and suse), this installs the LIO targetcli
    configuration utility.
    
    The targetcli package is available with recent versions of Fedora (22 &
    21), RHEL (7), Debian, Ubuntu, SLES (12) and openSUSE.
    
    "ceph-deploy install --all" continues to only offer installation of all
    Ceph components. i.e. targetcli is never installed by default.
    
    Signed-off-by: David Disseldorp <ddiss@suse.de>

--- ceph_deploy/install.py
+++ ceph_deploy/install.py
@@ -56,6 +56,7 @@ def detect_components(args, distro):
         'install_mds': 'ceph-mds',
         'install_mon': 'ceph-mon',
         'install_common': 'ceph-common',
+        'install_iscsi': 'targetcli',
     }
 
     if distro.is_rpm:
@@ -519,6 +520,13 @@ def make(parser):
         dest='install_all',
         action='store_true',
         help='install all ceph components (e.g. mon,osd,mds,rgw). This is the default',
+    )
+
+    version.add_argument(
+        '--iscsi',
+        dest='install_iscsi',
+        action='store_true',
+        help='install the LIO iSCSI target utility only',
     )
 
     version.add_argument(
