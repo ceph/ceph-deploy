3158303cfdea889f9c1d07e2fa84b2109fc00775
David Disseldorp <ddiss@suse.de>
2015-06-12 14:50:21 +0200

    suse/uninstall: use the zypper remove helper
    Signed-off-by: David Disseldorp <ddiss@suse.de>

--- ceph_deploy/hosts/suse/uninstall.py
+++ ceph_deploy/hosts/suse/uninstall.py
@@ -1,4 +1,4 @@
-from ceph_deploy.lib import remoto
+from ceph_deploy.util import pkg_managers
 
 
 def uninstall(conn, purge=False):
@@ -10,12 +10,4 @@ def uninstall(conn, purge=False):
         'librbd1',
         'ceph-radosgw',
         ]
-    cmd = [
-        'zypper',
-        '--non-interactive',
-        '--quiet',
-        'remove',
-        ]
-
-    cmd.extend(packages)
-    remoto.process.run(conn, cmd)
+    pkg_managers.zypper_remove(conn, packages)
