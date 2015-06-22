8a62ae3991b1b82819743283ff06df6149ae00ee
Travis Rhoden <trhoden@redhat.com>
2015-06-22 16:08:59 -0700

    [RM-11742] Tests for --verbose/--quiet flags
    Adds new parser dir in tests/, for testing parser behavior
    
    Signed-off-by: Travis Rhoden <trhoden@redhat.com>

---
+++ ceph_deploy/tests/parser/test_main.py
@@ -0,0 +1,31 @@
+import pytest
+
+from ceph_deploy.cli import get_parser
+
+
+class TestParserMain(object):
+
+    def setup(self):
+        self.parser = get_parser()
+
+    def test_verbose_true(self):
+        args = self.parser.parse_args('--verbose forgetkeys'.split())
+        assert args.verbose
+
+    def test_verbose_default_is_false(self):
+        args = self.parser.parse_args('forgetkeys'.split())
+        assert not args.verbose
+
+    def test_quiet_true(self):
+        args = self.parser.parse_args('--quiet forgetkeys'.split())
+        assert args.quiet
+
+    def test_quiet_default_is_false(self):
+        args = self.parser.parse_args('forgetkeys'.split())
+        assert not args.quiet
+
+    def test_verbose_quiet_are_mutually_exclusive(self, capsys):
+        with pytest.raises(SystemExit):
+            self.parser.parse_args('--verbose --quiet forgetkeys'.split())
+        out, err = capsys.readouterr()
+        assert 'not allowed with argument' in err
