4c3850af31064b027576fbf1379902e01a6ca95d
Travis Rhoden <trhoden@redhat.com>
2015-06-22 16:54:17 -0700

    [RM-11742] Remove test for bad subcommand from test_cli
    Now covered in parser/test_main.py
    
    Signed-off-by: Travis Rhoden <trhoden@redhat.com>

--- ceph_deploy/tests/test_cli.py
+++ ceph_deploy/tests/test_cli.py
@@ -13,15 +13,3 @@ def test_help(tmpdir, cli):
         assert 'usage: ceph-deploy' in result
         assert 'optional arguments:' in result
         assert 'commands:' in result
-
-
-def test_bad_command(tmpdir, cli):
-    with pytest.raises(cli.Failed) as err:
-        with cli(
-            args=['ceph-deploy', 'bork'],
-            stderr=subprocess.PIPE,
-            ) as p:
-            result = p.stderr.read()
-    assert 'usage: ceph-deploy' in result
-    assert err.value.status == 2
-    assert [f.basename for f in tmpdir.listdir()] == []
