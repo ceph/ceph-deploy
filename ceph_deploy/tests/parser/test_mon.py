4f3ed71e544cd11d79f38627b29f6450865bb172
Travis Rhoden <trhoden@redhat.com>
2015-06-24 14:46:07 -0700

    [RM-11742] Add tests mon create
    Signed-off-by: Travis Rhoden <trhoden@redhat.com>

--- ceph_deploy/tests/parser/test_mon.py
+++ ceph_deploy/tests/parser/test_mon.py
@@ -57,3 +57,27 @@ class TestParserMON(object):
     def test_mon_create_initial_keyrings_host_raises_err(self):
         with pytest.raises(SystemExit):
             self.parser.parse_args('mon create-initial test1'.split())
+
+    @pytest.mark.skipif(reason="http://tracker.ceph.com/issues/12151")
+    def test_mon_create_help(self, capsys):
+        with pytest.raises(SystemExit):
+            self.parser.parse_args('mon create --help'.split())
+        out, err = capsys.readouterr()
+        assert 'usage: ceph-deploy mon create' in out
+
+    def test_mon_create_keyrings_default_none(self):
+        args = self.parser.parse_args('mon create'.split())
+        assert args.keyrings is None
+
+    def test_mon_create_keyrings_custom_dir(self):
+        args = self.parser.parse_args('mon create --keyrings /tmp/keys'.split())
+        assert args.keyrings == "/tmp/keys"
+
+    def test_mon_create_single_host(self):
+        args = self.parser.parse_args('mon create test1'.split())
+        assert args.mon == ['test1']
+
+    def test_mon_create_multi_host(self):
+        hosts = ['host1', 'host2', 'host3']
+        args = self.parser.parse_args('mon create'.split() + hosts)
+        assert args.mon == hosts
