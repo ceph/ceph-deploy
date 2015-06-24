aa42e9e16947170e47710cf4851fa62341e633a9
Travis Rhoden <trhoden@redhat.com>
2015-06-24 15:12:34 -0700

    [RM-11742] Add argparse tests for mon add
    Signed-off-by: Travis Rhoden <trhoden@redhat.com>

--- ceph_deploy/tests/parser/test_mon.py
+++ ceph_deploy/tests/parser/test_mon.py
@@ -81,3 +81,32 @@ class TestParserMON(object):
         hosts = ['host1', 'host2', 'host3']
         args = self.parser.parse_args('mon create'.split() + hosts)
         assert args.mon == hosts
+
+    @pytest.mark.skipif(reason="http://tracker.ceph.com/issues/12151")
+    def test_mon_add_help(self, capsys):
+        with pytest.raises(SystemExit):
+            self.parser.parse_args('mon add --help'.split())
+        out, err = capsys.readouterr()
+        assert 'usage: ceph-deploy mon add' in out
+
+    def test_mon_add_address_default_none(self):
+        args = self.parser.parse_args('mon add test1'.split())
+        assert args.address is None
+
+    def test_mon_add_address_custom_addr(self):
+        args = self.parser.parse_args('mon add test1 --address 10.10.0.1'.split())
+        assert args.address == '10.10.0.1'
+
+    @pytest.mark.skipif(reason="http://tracker.ceph.com/issues/12151")
+    def test_mon_add_no_host_raises_err(self):
+        with pytest.raises(SystemExit):
+            self.parser.parse_args('mon add'.split())
+
+    def test_mon_add_one_host_okay(self):
+        args = self.parser.parse_args('mon add test1'.split())
+        assert args.mon == ["test1"]
+
+    @pytest.mark.skipif(reason="http://tracker.ceph.com/issues/12151")
+    def test_mon_add_multi_host_raises_err(self):
+        with pytest.raises(SystemExit):
+            self.parser.parse_args('mon add test1 test2'.split())
