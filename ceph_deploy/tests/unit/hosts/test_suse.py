01bd8282a7f59350fcb04bae3e59e9ddeb1e3dbb
David Disseldorp <ddiss@suse.de>
2015-06-29 18:46:19 +0200

    Add tests for component to SUSE package mapping
    Signed-off-by: David Disseldorp <ddiss@suse.de>

--- ceph_deploy/tests/unit/hosts/test_suse.py
+++ ceph_deploy/tests/unit/hosts/test_suse.py
@@ -1,4 +1,5 @@
 from ceph_deploy.hosts import suse 
+from ceph_deploy.hosts.suse.install import map_components
 
 class TestSuseInit(object):
     def setup(self):
@@ -23,3 +24,16 @@ class TestSuseInit(object):
         self.host.release = '13.1'
         init_type = self.host.choose_init()
         assert init_type == "systemd"
+
+class TestSuseMapComponents(object):
+    def test_valid(self):
+        pkgs = map_components(['ceph-osd', 'ceph-common', 'ceph-radosgw'])
+        assert 'ceph' in pkgs
+        assert 'ceph-common' in pkgs
+        assert 'ceph-radosgw' in pkgs
+        assert 'ceph-osd' not in pkgs
+
+    def test_invalid(self):
+        pkgs = map_components(['not-provided', 'ceph-mon'])
+        assert 'not-provided' not in pkgs
+        assert 'ceph' in pkgs
