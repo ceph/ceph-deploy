30959780c447452bf1533c023b1c327430d864a1
Travis Rhoden <trhoden@redhat.com>
2015-07-31 15:55:38 -0700

    [RM-12439] rhel: use Yum Class package manager
    Refs: #12439
    
    Signed-off-by: Travis Rhoden <trhoden@redhat.com>

--- ceph_deploy/hosts/__init__.py
+++ ceph_deploy/hosts/__init__.py
@@ -69,7 +69,7 @@ def get(hostname,
     module.conn = conn
     module.machine_type = machine_type
     module.init = module.choose_init()
-    if module.normalized_name in ['fedora', 'centos']:
+    if module.normalized_name in ['fedora', 'centos', 'redhat']:
         module.packager = module.get_packager(module)
     return module
 
--- ceph_deploy/hosts/rhel/__init__.py
+++ ceph_deploy/hosts/rhel/__init__.py
@@ -2,6 +2,7 @@ import mon  # noqa
 import pkg  # noqa
 from install import install, mirror_install, repo_install  # noqa
 from uninstall import uninstall  # noqa
+from ceph_deploy.util import pkg_managers
 
 # Allow to set some information about this distro
 #
@@ -17,3 +18,7 @@ def choose_init():
     Returns the name of a init system (upstart, sysvinit ...).
     """
     return 'sysvinit'
+
+
+def get_packager(module):
+    return pkg_managers.Yum(module)
