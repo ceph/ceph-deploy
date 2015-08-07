14fec138989b1fb31bdc727ce8dd67f061574a98
Travis Rhoden <trhoden@redhat.com>
2015-08-07 15:22:19 -0700

    [RM-12644] Kill TestRPM
    pkg_managers.rpm() is unused in the codebase.
    
    There are places that *could* use it, but I'd rather roll that into
    pkg_managers.RPMBaseManager at some point
    
    Signed-off-by: Travis Rhoden <trhoden@redhat.com>

--- ceph_deploy/tests/unit/util/test_pkg_managers.py
+++ ceph_deploy/tests/unit/util/test_pkg_managers.py
@@ -2,28 +2,6 @@ from mock import patch, Mock
 from ceph_deploy.util import pkg_managers
 
 
-class TestRPM(object):
-
-    def setup(self):
-        self.to_patch = 'ceph_deploy.util.pkg_managers.remoto.process.run'
-
-    def test_normal_flags(self):
-        fake_run = Mock()
-        with patch(self.to_patch, fake_run):
-            pkg_managers.rpm(Mock())
-            result = fake_run.call_args_list[-1]
-        assert result[0][-1] == ['rpm', '-Uvh']
-
-    def test_extended_flags(self):
-        fake_run = Mock()
-        with patch(self.to_patch, fake_run):
-            pkg_managers.rpm(
-                Mock(),
-                ['-f', 'vim'])
-            result = fake_run.call_args_list[-1]
-        assert result[0][-1] == ['rpm', '-Uvh', '-f', 'vim']
-
-
 class TestApt(object):
 
     def setup(self):
