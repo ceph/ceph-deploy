759634c7a1732cfb6cc06304c0f0c5288db2a92f
Travis Rhoden <trhoden@redhat.com>
2015-09-14 15:47:22 -0700

    [RM-13087] Fix unit tests
    Signed-off-by: Travis Rhoden <trhoden@redhat.com>

--- ceph_deploy/tests/unit/hosts/test_suse.py
+++ ceph_deploy/tests/unit/hosts/test_suse.py
@@ -1,29 +1,32 @@
+from mock import MagicMock
+
 from ceph_deploy.hosts import suse 
 from ceph_deploy.hosts.suse.install import map_components, NON_SPLIT_PACKAGES
 
 class TestSuseInit(object):
     def setup(self):
         self.host = suse
+        self.host.conn = MagicMock()
 
     def test_choose_init_default(self):
         self.host.release = None
         init_type = self.host.choose_init(self.host)
-        assert init_type == "systemd"
+        assert init_type.name == "systemd"
 
     def test_choose_init_SLE_11(self):
         self.host.release = '11'
         init_type = self.host.choose_init(self.host)
-        assert init_type == "sysvinit"
+        assert init_type.name == "sysvinit"
 
     def test_choose_init_SLE_12(self):
         self.host.release = '12'
         init_type = self.host.choose_init(self.host)
-        assert init_type == "systemd"
+        assert init_type.name == "systemd"
 
     def test_choose_init_openSUSE_13_1(self):
         self.host.release = '13.1'
         init_type = self.host.choose_init(self.host)
-        assert init_type == "systemd"
+        assert init_type.name == "systemd"
 
 class TestSuseMapComponents(object):
     def test_valid(self):
