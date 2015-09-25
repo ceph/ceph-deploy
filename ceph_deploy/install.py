613cb976e9ba4e9088cfb69a9037677590ece32f
Loic Dachary <ldachary@redhat.com>
2015-09-25 19:44:44 +0200

    install --no-check-package-signatures for test purposes
    When installing from repositories that are created dynamically within a
    lab, verifying the package signatures can be skipped. The
    --no-check-package-signatures configures the repositories to not require
    package signatures verification during installation.
    
    http://tracker.ceph.com/issues/13249 Fixes: #13249
    
    Signed-off-by: Loic Dachary <loic@dachary.org>

--- ceph_deploy/install.py
+++ ceph_deploy/install.py
@@ -183,6 +183,7 @@ def install(args):
                 version,
                 args.adjust_repos,
                 components=components,
+                no_check_packages_signatures=args.no_check_packages_signatures,
             )
 
         # Check the ceph version we just installed
@@ -597,6 +598,12 @@ def make(parser):
         dest='gpg_url',
         help='specify a GPG key URL to be used with custom repos\
                 (defaults to ceph.com)'
+    )
+
+    parser.add_argument(
+        '--no-check-packages-signatures',
+        action='store_true',
+        help='do not check the package signatures',
     )
 
     parser.set_defaults(
