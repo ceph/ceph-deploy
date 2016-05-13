4cd99f5a0651c075072a187c9867b0737c6fc4a1
Owen Synge <osynge@suse.com>
2016-05-13 11:44:48 +0200

    Allow rgw port to be defined by ceph-deploy
    We need to set the port for the rgw.
    
    Signed-off-by:  Owen Synge <osynge@suse.com>

--- ceph_deploy/rgw.py
+++ ceph_deploy/rgw.py
@@ -152,13 +152,12 @@ def rgw_create(args):
 
     # Update the config file
     changed_cfg = False
-    for hostname, name in args.rgw:
+    for hostname, name, port in args.rgw:
         if not name.startswith('rgw.'):
             msg = "rgw name '%s' does not start with 'rgw.'" % (name)
             LOG.error(msg)
             raise RuntimeError(msg)
         enitity = 'client.{name}'.format(name=name)
-        port = 7480
         if cfg.has_section(enitity) is False:
             cfg.add_section(enitity)
             changed_cfg = True
@@ -174,7 +173,6 @@ def rgw_create(args):
                 msg = "exisiting rgw '%s:%s' has a different hostname" % (hostname, name)
                 LOG.error(msg)
                 raise RuntimeError(msg)
-
         if cfg.has_option(enitity,'rgw_dns_name') is False:
             cfg.set(enitity, 'rgw_dns_name', hostname)
             changed_cfg = True
@@ -206,7 +204,7 @@ def rgw_create(args):
         with open(cfg_path, 'wb') as configfile:
             cfg.write(configfile)
 
-    for hostname, name in args.rgw:
+    for hostname, name, port in args.rgw:
         try:
             distro = hosts.get(hostname, username=args.username)
             rlogger = distro.conn.logger
@@ -240,9 +238,9 @@ def rgw_create(args):
             distro.conn.exit()
             LOG.info(
                 ('The Ceph Object Gateway (RGW) is now running on host %s and '
-                 'default port %s'),
+                 'port %s'),
                 hostname,
-                '7480'
+                port
             )
         except RuntimeError as e:
             LOG.error(e)
@@ -321,9 +319,8 @@ def rgw_delete(args):
 
     # Check if config needs to be changed
     changed_cfg = False
-    for hostname, name in args.rgw:
+    for hostname, name, port in args.rgw:
         enitity = 'client.{name}'.format(name=name)
-        port = 7480
         if cfg.has_section(enitity) is True:
             cfg.remove_section(enitity)
             changed_cfg = True
@@ -337,7 +334,8 @@ def rgw_delete(args):
             raise RuntimeError(msg)
 
     changed_cfg = False
-    for hostname, name in args.rgw:
+
+    for hostname, name, port in args.rgw:
         try:
             distro = hosts.get(hostname, username=args.username)
             LOG.info(
@@ -377,7 +375,7 @@ def rgw_delete(args):
         with open(cfg_path, 'wb') as configfile:
             cfg.write(configfile)
         # now distribute
-        for hostname, name in args.rgw:
+        for hostname, name, port in args.rgw:
             try:
                 distro = hosts.get(hostname, username=args.username)
                 LOG.info(
@@ -414,10 +412,14 @@ def rgw(args):
 def colon_separated(s):
     host = s
     name = 'rgw.' + s
-    if s.count(':') == 1:
+    port = '7480'
+    delimiter_count = s.count(':')
+    if delimiter_count == 1:
         (host, name) = s.split(':')
+    if delimiter_count == 2:
+        (host, name, port) = s.split(':')
     name = validate.alphanumeric(name)
-    return (host, name)
+    return (host, name, port)
 
 
 @priority(30)
@@ -433,11 +435,10 @@ def make(parser):
         )
     rgw_create.add_argument(
         'rgw',
-        metavar='HOST[:NAME]',
+        metavar='HOST[:NAME][:PORT]',
         nargs='+',
         type=colon_separated,
-        help='host (and optionally the daemon name) to deploy on. \
-                NAME is automatically prefixed with \'rgw.\'',
+        help='host (and optionally the daemon name and port) to deploy on.',
         )
     rgw_parser.add_parser(
         'list',
