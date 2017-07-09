ad4e1cab431ccd8fa5b820d5c6bf564a24fb94e3
Yaguang Tang <tangyg2@lenovo.com>
2017-07-09 22:52:46 +0800

    Support deploy on Debian 9 stretch

--- ceph_deploy/hosts/remotes.py
+++ ceph_deploy/hosts/remotes.py
@@ -16,6 +16,7 @@ def platform_information(_linux_distribution=None):
     distro, release, codename = linux_distribution()
     if not codename and 'debian' in distro.lower():  # this could be an empty string in Debian
         debian_codenames = {
+            '9': 'stretch',
             '8': 'jessie',
             '7': 'wheezy',
             '6': 'squeeze',
