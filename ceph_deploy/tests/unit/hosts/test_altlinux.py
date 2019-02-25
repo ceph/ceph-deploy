f6918ba87c36ad655191153aa991f5ceeb9eebe7
Dimitri Savineau <dsavinea@redhat.com>
2019-02-25 13:12:05 -0500

    test_altlinux: Remove print statement
    The commit 6190bb2 introduced an unnecessary print statement in the
    altlinux tests.
    
    Signed-off-by: Dimitri Savineau <dsavinea@redhat.com>

--- ceph_deploy/tests/unit/hosts/test_altlinux.py
+++ ceph_deploy/tests/unit/hosts/test_altlinux.py
@@ -4,7 +4,6 @@ from ceph_deploy.hosts.alt.install import map_components, NON_SPLIT_PACKAGES
 class TestALTMapComponents(object):
     def test_valid(self):
         pkgs = map_components(NON_SPLIT_PACKAGES, ['ceph-osd', 'ceph-common', 'ceph-radosgw'])
-        print(pkgs)
         assert 'ceph' in pkgs
         assert 'ceph-common' in pkgs
         assert 'ceph-radosgw' in pkgs
