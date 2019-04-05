837ef70b235bd5810846c64c0de58151a21c013d
Sage Weil <sage@redhat.com>
2019-04-05 10:03:57 -0500

    default to nautilus

--- ceph_deploy/install.py
+++ ceph_deploy/install.py
@@ -17,7 +17,7 @@ def sanitize_args(args):
     not well suited for.
     """
     if args.release is None:
-        args.release = 'mimic'
+        args.release = 'nautilus'
         args.default_release = True
 
     # XXX This whole dance is because --stable is getting deprecated
