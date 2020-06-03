ceph/ceph-deploy

commit b611bdf863871f134d2f982ae5e0e70a498c0a99

cephdeploy.conf: use more meaningful example apt repo url
Signed-off-by: Yuan Zhou <yuan.zhou@intel.com>

ceph_deploy/conf/cephdeploy.py
@@ -28,29 +28,29 @@ cd_conf_template = """
 
 # yum repos:
 # [myrepo]
-# baseurl = https://user:pass@example.org/rhel6
-# gpgurl = https://example.org/keys/release.asc
+# baseurl = http://gitbuilder.ceph.com/ceph-rpm-centos7-x86_64-basic/ref/hammer
+# gpgurl = https://git.ceph.com/?p=ceph.git;a=blob_plain;f=keys/autobuild.asc
 # default = True
 # extra-repos = cephrepo  # will install the cephrepo file too
 #
 # [cephrepo]
 # name=ceph repo noarch packages
-# baseurl=http://ceph.com/rpm-emperor/el6/noarch
+# baseurl=http://download.ceph.com/rpm-hammer/el6/noarch
 # enabled=1
 # gpgcheck=1
 # type=rpm-md
-# gpgkey={gpgurl}
+# gpgkey=https://git.ceph.com/?p=ceph.git;a=blob_plain;f=keys/release.asc
 
 # apt repos:
 # [myrepo]
-# baseurl = https://user:pass@example.org/
-# gpgurl = https://example.org/keys/release.asc
+# baseurl = http://gitbuilder.ceph.com/ceph-deb-trusty-x86_64-basic/ref/hammer
+# gpgurl = https://git.ceph.com/?p=ceph.git;a=blob_plain;f=keys/autobuild.asc
 # default = True
 # extra-repos = cephrepo  # will install the cephrepo file too
 #
 # [cephrepo]
-# baseurl=http://ceph.com/rpm-emperor/el6/noarch
-# gpgkey={gpgurl}
+# baseurl=http://download.ceph.com/debian-hammer
+# gpgkey=https://git.ceph.com/?p=ceph.git;a=blob_plain;f=keys/release.asc
 """.format(gpgurl=gpg.url('release'))
 
 

commit 0a754228dcc385edf45ff4bcb3a03e69d13d856d

conf/cephdeploy.py: use gpg.url() for cephdeploy.conf template
see also e12d1c17c30ee17850dfc2ab2b9716d9f5f8ca6d,
it's a regression introduced by b611bdf863871f134d2f982ae5e0e70a498c0a99

Signed-off-by: Kefu Chai <kchai@redhat.com>

ceph_deploy/conf/cephdeploy.py
@@ -29,7 +29,7 @@ cd_conf_template = """
 # yum repos:
 # [myrepo]
 # baseurl = http://gitbuilder.ceph.com/ceph-rpm-centos7-x86_64-basic/ref/hammer
-# gpgurl = https://download.ceph.com/keys/autobuild.asc
+# gpgurl = {gpg_url_autobuild}
 # default = True
 # extra-repos = cephrepo  # will install the cephrepo file too
 #
@@ -39,19 +39,20 @@ cd_conf_template = """
 # enabled=1
 # gpgcheck=1
 # type=rpm-md
-# gpgkey=https://download.ceph.com/keys/release.asc
+# gpgkey={gpg_url_release}
 
 # apt repos:
 # [myrepo]
 # baseurl = http://gitbuilder.ceph.com/ceph-deb-trusty-x86_64-basic/ref/hammer
-# gpgurl = https://download.ceph.com/keys/autobuild.asc
+# gpgurl = {gpg_url_autobuild}
 # default = True
 # extra-repos = cephrepo  # will install the cephrepo file too
 #
 # [cephrepo]
 # baseurl=http://download.ceph.com/debian-hammer
-# gpgkey=https://download.ceph.com/keys/release.asc
-""".format(gpgurl=gpg.url('release'))
+# gpgkey={gpg_url_release}
+""".format(gpg_url_release=gpg.url('release'),
+           gpg_url_autobuild=gpg.url('autobuild'))
 
 
 def location():